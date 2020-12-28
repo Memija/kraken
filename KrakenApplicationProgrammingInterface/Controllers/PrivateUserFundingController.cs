a07411bbf1e3c021fff206d5df82ea3168594985
Memija <anelmemija@gmail.com>
2020-12-28 19:24:36 +0100

    GetDepositMethods method.

---
+++ KrakenApplicationProgrammingInterface/Controllers/PrivateUserFundingController.cs
@@ -0,0 +1,44 @@
+﻿using KrakenLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KrakenApplicationProgrammingInterface.Controllers
+{
+    /// <summary>
+    /// Private user funding
+    /// </summary>
+    /// <remarks>
+    /// This is a tentative funding API and may be updated in the future.
+    /// </remarks>
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/private-user-funding")]
+    public class PrivateUserFundingController : ControllerBase
+    {
+        private readonly Client client;
+
+        public PrivateUserFundingController(Client clientInstance)
+        {
+            client = clientInstance;
+        }
+
+        /// <summary>
+        /// Get deposit methods
+        /// </summary>
+        /// <returns>
+        /// associative array of deposit methods
+        /// </returns>
+        /// <see cref="DepositMethod"/>
+        /// <remarks>
+        /// https://www.kraken.com/features/api#deposit-methods
+        /// </remarks>
+        [HttpGet("get-deposit-methods")]
+        public async Task<Response<List<DepositMethod>>> GetDepositMethods()
+        {
+            string getDepositMethodsEndpoint = "DepositMethods";
+
+            return await client.GetPrivateUserData<List<DepositMethod>>($"{getDepositMethodsEndpoint}{this.HttpContext.Request.QueryString.Value}");
+        }
+    }
+}
---
+++ KrakenLibrary/Models/DepositMethod.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace KrakenLibrary.Models
+{
+    /// <summary>
+    /// Deposit method
+    /// </summary>
+    public class DepositMethod
+    {
+        /// <summary>
+        /// name of deposit method
+        /// </summary>
+        [JsonProperty("method")]
+        public string Method { get; set; }
+
+        /// <summary>
+        /// maximum net amount that can be deposited right now, or false if no limit
+        /// </summary>
+        [JsonProperty("limit")]
+        public string Limit { get; set; }
+
+        /// <summary>
+        /// amount of fees that will be paid
+        /// </summary>
+        [JsonProperty("fee")]
+        public string Fee { get; set; }
+
+        /// <summary>
+        /// whether or not method has an address setup fee(optional)
+        /// </summary>
+        [JsonProperty("address-setup-fee")]
+        public string AddressSetupFee { get; set; }
+    }
+}
